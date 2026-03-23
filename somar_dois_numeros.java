c487f0899c61073b4c8581ed62cf2ae70ca9b60c
Evelyn <0000122094354xsp@al.educacao.sp.gov.br>
2026-03-23 14:20:26 -0300

    Add files via upload

---
+++ somar_dois_numeros.java
@@ -0,0 +1,18 @@
+void main(){
+  //entradas
+
+   IO.print("digite a temperatura em graus celsius");
+   String c = IO.readln();
+
+
+
+  //processamentos
+   float graus_c = Float.parseFloat(c);
+   float f = c *9 / 5 + 32;
+
+
+  //saidas
+   IO.print;
+
+
+}
